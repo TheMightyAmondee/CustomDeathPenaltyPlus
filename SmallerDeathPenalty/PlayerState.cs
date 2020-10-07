7da804a36483e2df15f3c9889b64b29650cd4970
Amelie <amelie.parkinson.bates@gmail.com>
2020-10-07 20:36:54 +1100

    Initial commit - saves money

---
+++ SmallerDeathPenalty/PlayerState.cs
@@ -0,0 +1,17 @@
+﻿using StardewValley;
+using StardewValley.Tools;
+using System.Collections.Generic;
+
+namespace SmallerDeathPenalty
+{
+    //Tracks player's funds
+    internal class PlayerState
+    {
+        public int money;
+
+        public PlayerState(int m)
+        {
+            this.money = m;
+        }
+    }
+}
---
+++ SmallerDeathPenalty/PlayerStateSaver.cs
@@ -0,0 +1,22 @@
+﻿using StardewValley;
+using StardewValley.Locations;
+
+namespace SmallerDeathPenalty
+{
+    internal class PlayerStateSaver
+    {
+        public static PlayerState state;
+
+        // Saves player's current funds
+        public static void Save()
+        {
+            state = new PlayerState(Game1.player.Money);
+        }
+
+        //Load funds
+        public static void Load()
+        {
+            Game1.player.Money = state.money;
+        }
+    }
+}
