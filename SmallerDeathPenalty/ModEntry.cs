d9a0527a0a2a9d49313a18f14572e31c50f8ef19
Amelie <amelie.parkinson.bates@gmail.com>
2020-10-18 17:16:54 +1100

    Fixed strings to reflect config values

--- SmallerDeathPenalty/ModEntry.cs
+++ SmallerDeathPenalty/ModEntry.cs
@@ -34,38 +34,39 @@ namespace SmallerDeathPenalty
         {
             var editor = asset.AsDictionary<string, string>().Data;
 
-            //Does the PlayerStateSaver not exist or is the player's money greater than 10,000g?
-            if (PlayerStateSaver.state == null || PlayerStateSaver.state.money * (1 - Config.RestoreMoneyPercentage) > Config.MoneyLossCap)
+            //Does the PlayerStateSaver not exist or is the config invalid?
+            if (PlayerStateSaver.state == null || Config.RestoreMoneyPercentage > 1 || Config.RestoreMoneyPercentage < 0)
             {
-                //Use default if config is invalid
-                if (Config.RestoreMoneyPercentage > 1 || Config.RestoreMoneyPercentage < 0)
+                editor["Event.cs.1068"] = "Dr. Harvey charged me 500g for the hospital visit. ";
+                editor["Event.cs.1058"] = "I seem to have lost 500g";
+            }
+            
+            //Edit strings to refernce objects
+            else
+            {
+                if (PlayerStateSaver.state.money * (1 - Config.RestoreMoneyPercentage) > Config.MoneyLossCap)
                 {
-                    editor["Event.cs.1068"] = "Dr. Harvey charged me 500g for the hospital visit. ";
-                    editor["Event.cs.1058"] = "I seem to have lost 500g";
-                }
-                else
-                {
-                    //Edit events to reflect capped amount lost, default
+                    //Edit events to reflect capped amount lost
                     editor["Event.cs.1068"] = $"Dr. Harvey charged me {Config.MoneyLossCap}g for the hospital visit. ";
                     editor["Event.cs.1058"] = $"I seem to have lost {Config.MoneyLossCap}g";
                 }
-            }
-            else
-            {
-                //Edit events to reflect discounted amount lost
-                editor["Event.cs.1068"] = $"Dr. Harvey charged me {PlayerStateSaver.state.money - (int)Math.Round(PlayerStateSaver.state.money * Config.RestoreMoneyPercentage)}g for the hospital visit. ";
-                editor["Event.cs.1058"] = $"I seem to have lost {PlayerStateSaver.state.money - (int)Math.Round(PlayerStateSaver.state.money * Config.RestoreMoneyPercentage)}g.";
-            }
-            if (Config.RestoreItems == true)
-            {
-                //Remove unnecessary strings
-                editor["Event.cs.1060"] = "";
-                editor["Event.cs.1061"] = "";
-                editor["Event.cs.1062"] = "";
-                editor["Event.cs.1063"] = "";
-                editor["Event.cs.1071"] = "";
-            }
-               
+                else
+                {
+                    //Edit events to reflect discounted amount lost
+                    editor["Event.cs.1068"] = $"Dr. Harvey charged me {PlayerStateSaver.state.money - (int)Math.Round(PlayerStateSaver.state.money * Config.RestoreMoneyPercentage)}g for the hospital visit. ";
+                    editor["Event.cs.1058"] = $"I seem to have lost {PlayerStateSaver.state.money - (int)Math.Round(PlayerStateSaver.state.money * Config.RestoreMoneyPercentage)}g.";
+                }
+
+                if (Config.RestoreItems == true)
+                {
+                    //Remove unnecessary strings
+                    editor["Event.cs.1060"] = "";
+                    editor["Event.cs.1061"] = "";
+                    editor["Event.cs.1062"] = "";
+                    editor["Event.cs.1063"] = "";
+                    editor["Event.cs.1071"] = "";
+                }
+            }   
         }
         public override void Entry(IModHelper helper)
         {
@@ -104,7 +105,6 @@ namespace SmallerDeathPenalty
                 //Reset PlayerStateSaver
                 PlayerStateSaver.state = null;
 
-                this.Monitor.Log("Player state restored...", LogLevel.Debug);
                 //Show if config values are invalid
                 if (Config.RestoreMoneyPercentage > 1 || Config.RestoreMoneyPercentage < 0)
                 {
@@ -118,6 +118,8 @@ namespace SmallerDeathPenalty
                 {
                     this.Monitor.Log("HealthtoRestorePercentage is an invalid value, using default instead... (Is the value a decimal between 0 and 1?)", LogLevel.Debug);
                 }
+
+                this.Monitor.Log("Player state restored...", LogLevel.Debug);
             }
         }
     }
