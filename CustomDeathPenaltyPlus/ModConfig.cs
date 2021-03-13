92b1feaca7113ecb2ebc5bc4ce02f85d936c0994
Amelie <amelie.parkinson.bates@gmail.com>
2021-03-13 14:28:28 +1100

    fixed config

--- CustomDeathPenaltyPlus/ModConfig.cs
+++ CustomDeathPenaltyPlus/ModConfig.cs
@@ -28,7 +28,6 @@ namespace CustomDeathPenaltyPlus
         internal class OtherChanges
         {
             public bool WakeupNextDayinClinic { get; set; } = false;
-            public int ForetMineLevels { get; set; } = 0;
             public int HarveyFriendshipChange { get; set; } = 0;
             public int MaruFriendshipChange { get; set; } = 0;
         }
